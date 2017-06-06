6065fe6dacf2eb283e378f14db5c37496dfcf425
Mathias Taylor <mathiasjtaylor@gmail.com>
2017-06-05 21:10:02 -0700

    Add files via upload

---
+++ LaxBookJS.js
@@ -0,0 +1,29 @@
+
+	
+var pos =0;
+
+
+var t= setInterval(h3move,10);
+var x= document.getElementById('h3');
+var y= document.getElementById("but");
+var p=document.getElementById("p");
+function move()
+{
+	
+	alert("alert");
+	
+}
+function mouse()
+{
+	p.style.color= 'yellow';
+}
+
+/*if(pos>150)
+	{
+		clearInterval(t);
+	}
+	else{
+		pos+=1;
+		x.stlye.left= pos+'px';
+	}
+	*/
